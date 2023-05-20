3582124eb94c4a81b14eee7b84fd536b097e5391
Albina Anistratenko <albuska2013@gmail.com>
2023-05-20 20:32:11 +0300

    Decorate home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,36 +1,19 @@
 import MyPhoto from '../components/assets/photo.jpg';
-// import Typical from 'react-typical';
+import TypeWriterEffect from 'react-typewriter-effect';
 
 const Home = () => {
   return (
     <div style={{ textAlign: 'center' }}>
       <img src={MyPhoto} alt="Photo" width="350" style={{ paddingTop: 20 }} />
-      <h1>
-        {/* <Typical
-          steps={[
-            "Hi, I'm Albina Anistratenko. I'm a begginer frontend developer. I want to show you my technical assignment. I invite you to go to the next page. Have a good day!",
-            2000,
-          ]}
-          loop={Infinity}
-          wrapper="b"
-        /> */}
-        {/* <Typical
-          steps={[
-            "Hi, I'm Albina Anistratenko.",
-            2000,
-            "I'm a begginer frontend developer.",
-            2000,
-            'I want to show you my technical assignment.',
-            2000,
-            'I invite you to go to the next page.',
-            2000,
-            'Have a good day!',
-            2000,
-          ]}
-          loop={Infinity}
-          wrapper="b"
-        /> */}
-      </h1>
+      <p>
+        <TypeWriterEffect
+          startDelay={100}
+          cursorColor="black"
+          text="Hi, I'm Albina Anistratenko. I'm a begginer frontend developer. I want to show you my technical assignment. I invite you to go to the next page. Have a good day!"
+          typeSpeed={100}
+          eraseSpeed={100}
+        />
+      </p>
     </div>
   );
 };
