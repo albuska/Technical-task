a0b567a59b1b86f5f2dbc76f276c1cf5af4c6405
albuska <albuska@users.noreply.github.com>
2023-05-20 17:36:28 +0000

    deploy: 3829cb8ff38e1cf396cbce6bd7d3b60158ee4d50

---
+++ assets/NotFound-db5c2430.js
@@ -0,0 +1,1 @@
+import{j as t,L as e}from"./index-b4b9aec9.js";const o="/technical-task/assets/notFound-f884bf80.jpg",n=()=>t.jsxs("div",{style:{marginTop:20,textAlign:"center"},children:[t.jsx(e,{to:"/",children:"Back to the home page"}),t.jsx("img",{src:o,alt:"NotFoundPage",width:500,style:{marginTop:20}})]});export{n as default};
